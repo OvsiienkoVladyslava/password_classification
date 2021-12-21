f4995e97e14e23f8e68340eb0b23127324ac45ef
OvsiienkoVladyslava <vladyslava.ovsiienko@gmail.com>
2021-12-21 23:56:26 +0200

    Added function for predict. Updated saved model.

--- classify.py
+++ classify.py
@@ -1,6 +1,8 @@
 import argparse
 import pandas as pd
 import numpy as np
+import pickle
+import xgboost
 
 from data_preprocessing import extract_characters
 
@@ -15,24 +17,33 @@ def process_input(options: argparse.Namespace):
 
     predictions_frame = data.copy()
     predictions_frame['strength'] = predictions
+    predictions_frame['strength'] = predictions_frame['strength'].map({
+        0: 'weak',
+        1: 'medium',
+        2: 'strong'
+    })
 
-    if options.save_txt:
-        predictions_frame.to_csv(options.save_path, index=False)
+    if options.save_file:
+        predictions_frame.to_csv(options.save_path + '/predictions.csv', index=False)
     else:
         pd.set_option('display.max_rows', None)
         print(predictions_frame)
 
 
 def predict(input_data: np.ndarray):
-    pass
+    xgb_model_loaded = xgboost.XGBClassifier()
+    xgb_model_loaded.load_model('./xgbclassifier')
+    predictions = xgb_model_loaded.predict(input_data)
+
+    return predictions
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', type=str, default='', help='file with passwords which start from new line')
-    parser.add_argument('--save-txt', type=bool, default=False,
-                        help='to save or not to save predictions in .csv (bool)')
-    parser.add_argument('--save-path', type=str, default='', help='path where to save predicted classes')
+    parser.add_argument('--save-file', type=int, default=0,
+                        help='to save or not to save predictions in .csv (1 - yes, 0 - no)')
+    parser.add_argument('--save-path', type=str, default='.', help='path where to save predicted classes file '
+                                                                  'predictions.csv')
     opt = parser.parse_args()
-
     process_input(opt)
